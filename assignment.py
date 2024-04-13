c2dab651e6ca5da4d7eae42c58dd9df8ea171b2b
SangayChozom <02230106.cst@rub.edu.bt>
2024-04-13 06:44:24 +0530

    adding new file

--- assignment.py
+++ assignment.py
@@ -1,26 +1,27 @@
 def read_input_file(file_path):
     
-    #Reads the input file and returns a list of tuples containing structure, outcomes for each round.
+    #Initialize empty list to store number of rounds played
     num_rounds = []
-    with open(file_path, 'r') as file:
+    with open(file_path, 'r') as file: #opening input file
         for line in file:
-            shape, outcome = line.strip().split()
-            num_rounds.append((shape, outcome))
+            shape, outcome = line.strip().split() #split each line and extract
+            num_rounds.append((shape, outcome)) #append shape, outcome
     return num_rounds
 
 def calculate_score(num_rounds):
 
     #Compute the total score based on the number of rounds played.
-    score = 0
+    total_score = 0
     for shape, outcome in num_rounds:
         shape_score = {"A": 1, "B": 2, "C": 3}.get(shape, 0)
         outcome_score = {"X": 0, "Y": 3, "Z": 6}.get(outcome, 0)
-        score += shape_score + outcome_score  
+        total_score += shape_score + outcome_score  
 
-    return score
+    return total_score
 
-# Main program
+# calling read_input functions to read file extracting number of rounds assigned to new variable.
 file_path =  "input_6_cap1.txt"  # Replace with the path to your input file
 num_rounds = read_input_file(file_path)
 sum_score = calculate_score(num_rounds)
+#print total score
 print("Total Score:", sum_score)
